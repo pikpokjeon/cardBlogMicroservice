278cedadc4b3ab2418a850d56e37dfb0aa582315
YaejinJeon <pikpokjeon@gmail.com>
2020-07-30 23:00:24 +0900

    reupload client-react folder

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,35 @@
+import React, {
+  useState
+} from 'react'
+import axios from 'axios';
+
+export default ({postId}) => {
+
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content
+    });
+
+    setContent('');
+
+  };
+  return ( 
+  <div>
+      <form onSubmit = { onSubmit} >
+      < div className = "form-group" >
+
+      <label > NewComment </label>  <
+      input value = {
+        content
+      }
+      onChange = {
+        e => setContent(e.target.value)
+      }
+      className = "form-control" / >
+      </div> < button className = "btn btn-primary g" > submit < /button> < / form >
+
+      </div> )};
